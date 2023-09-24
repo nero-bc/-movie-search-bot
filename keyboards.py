01be6c17a83691148ff4e0a2db059cdc49602f6a
nero-bc <126649764+nero-bc@users.noreply.github.com>
2023-09-24 21:30:30 +0530

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -4,7 +4,7 @@ from aiogram.types import InlineKeyboardButton,InlineKeyboardMarkup
 
 def movie_information():
     keyboard=InlineKeyboardMarkup()
-    search=InlineKeyboardButton(text="Поиск фильмов",callback_data="search")
+    search=InlineKeyboardButton(text="Search Movies",callback_data="search")
     keyboard.add(search)
     return keyboard
     
